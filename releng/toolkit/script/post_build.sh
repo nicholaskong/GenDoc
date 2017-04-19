c8a38a07b703871a4003b8035d04b7f141972de7
Antonio Campesino <antonio.campesino.robles@ericsson.com>
2017-04-19 13:54:33 +0200

    Adding support to publish the latest nightly build update-site to the https:/download.eclipse.org/gendoc/updates/nightly/latest
    Change-Id: I04adb3fda49626ee7fa537c14a9cf8bfc3fab95c

---
+++ releng/toolkit/script/post_build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+WORKSPACE=/cygdrive/c/Projects/GenDoc4E/git/org.eclipse.gendoc/
+p2UpdateSite=$WORKSPACE/releng/org.eclipse.gendoc.update-site/target
+updateSite=$WORKSPACE
+nightlyDownloadSrc=$WORKSPACE/releng/toolkit/downloads/updates/nightly/latest
+nightlyDownloadTarget=/home/data/httpd/download.eclipse.org/gendoc/updates/nightly/latest
+timestamp=$(date +%s000)
+
+rm -rf $updateSite/repository
+rm -rf $updateSite/*.zip
+mkdir -p $updateSite/repository
+mv $p2UpdateSite/repository $updateSite
+mv $p2UpdateSite/*.zip $updateSite
+
+rm -rf $nightlyDownloadTarget
+mkdir -p $nightlyDownloadTarget
+cp -rf $nightlyDownloadSrc $nightlyDownloadTarget/..
+
+# Replace the timestamp in the repo files 
+sed -i "s/!!__TIMESTAMP__!!/${timestamp}/" $nightlyDownloadTarget/compositeContent.xml 
+sed -i "s/!!__TIMESTAMP__!!/${timestamp}/" $nightlyDownloadTarget/compositeArtifacts.xml 
